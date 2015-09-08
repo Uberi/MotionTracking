720d52518506d27329cee129fa6e3d967449dcfa
Uberi <azhang9@gmail.com>
2015-09-08 00:45:41 -0400

    New functionality: select all the empties, and have the script match them up automatically.

--- README.md
+++ README.md
@@ -10,11 +10,9 @@ Rationale
 
 Blender's standard 2D and 3D tracking is pretty effective at what it was designed to do. However, there are limits on the quality of the 3D tracking done from only one camera angle.
 
-A good analogy is trying to determine how far away something is by looking at it with just one eye. Is it small and close, or big and far? Human beings can use contextual cues to estimate the distance, but not only is this inaccurate, it is not very practical to do with computers.
+If we use multiple cameras put in different places, we can obtain depth perception, though we can't use this directly in Blender. This addon adds the ability to resolve tracking points made from multiple cameras together into a single scene.
 
-If we use multiple cameras put in different places, we can obtain depth perception, though we can't use this directly in Blender. This is the purpose of this addon.
-
-With this addon, the user performs 2D tracking with two or more cameras, then combines these 2D tracks into the 3D motion of each track. With the 3D tracked data, we can control rigs, reconstruct scenes, and greenscreen with better accuracy than ever before.
+Essentially, this addon implements 3D tracking using multiple 2D tracks. The user performs 2D tracking with two or more cameras, then combines these 2D tracks into a 3D animation. With the 3D tracked data, we can control rigs, reconstruct scenes, and greenscreen with better accuracy than ever before.
 
 Installation
 ------------
@@ -22,7 +20,7 @@ Installation
 The process is the same as any other Blender addon:
 
 1. Open the User Preferences window (Ctrl + Alt + U) to the "Addons" tab.
-2. Press "Install from File...".
+2. Press "Install from File..." at the bottom of the window.
 3. In the resulting file browser pane, select `Resolve Camera Tracks.py`.
 4. The addon entry should appear in the user preferences window.
 5. Check the checkbox at the top right of the entry, beside the running man icon.
@@ -37,18 +35,18 @@ The following is a recommended workflow for working with the addon:
 1. In the real world, mark a point on the ground as the real reference point. This is the center of the stage, so to speak.
 2. Pick a point in Blender as the virtual reference point. The origin is generally a good choice.
 3. Set up two or more cameras facing the reference point. For two cameras, the best setup is having the cameras facing 90 degrees to each other.
-4. In Blender, add the same number of cameras, and set their properties such as sensor size to the same values as the real-world cameras. These values can often be found online by searching for the camera model, or in the user manual for the camera.
-5. Measure and record the distance and angle from each real camera to the reference point.
-6. Move the Blender cameras so that they are the same distance and angle from the virtual reference point as their associated real cameras are from the real reference point.
-7. Start recording with both cameras.
+4. In Blender, add the same number of cameras, and set their properties such as sensor size and focal length to the same values as the real-world cameras. These values can often be found online by searching for the camera model online, or in the camera user manual.
+5. Measure and record the location and orientation of each real camera relative to the reference point.
+6. Move the Blender cameras so that they are at the same location and orientation relative to the virtual reference point as their associated real cameras.
+7. Start recording with all cameras. This doesn't have to be at exactly the same time, but they do need to be matched up later.
 8. Begin the performance near the reference point, ensuring that points of interest are visible to the cameras as much as possible.
-9. Stop recording.
-10. Synchronize and trim the video footage from all cameras with a video editor such as Avidemux, or use Blender for this.
-11. Ensure the video footage from all cameras all start at the same moment in real time.
-12. In Blender, track the points of interest using the motion tracker, ensuring the camera settings such as focal length are set to the same as the real cameras.
-13. Use `Movie Clip Editor > Reconstruction > Link Empty to Track` for each track, making sure they are associated with the correct Blender camera.
-14. Follow the steps in the During section below, under the Usage heading, for each real-world point tracked.
-15. You should now have a set of Empty objects that track in 3D the locations of the real-world markers. An example usage would be using these as hooks for a rig.
+9. Stop recording with all cameras.
+10. Synchronize and trim the video footage from all cameras with a video editor such as Avidemux, or do this within Blender.
+11. Ensure the video footage from all cameras match up in real time.
+12. In Blender, track the points of interest using the motion tracker, ensuring the camera settings such as focal length are set to the same as the real cameras, and that tracks for any given feature have the same name across all videos.
+13. For each video, select all the tracks and use `Movie Clip Editor > Reconstruction > Link Empty to Track`, making sure they are associated with the correct Blender camera.
+14. Select all the generated empties, and invoke `View3D > Object > Resolve Camera Tracks`.
+15. You should now have a set of Empty objects that track in 3D the locations of the real-world markers. These can now be used for animation, such as setting these as hooks for a rig.
 
 Usage
 -----
@@ -57,41 +55,43 @@ The operator is accessible via `View3D > Object > Resolve Camera Tracks`, or `Se
 
 ### Before
 
-* The video footage is shot from two or more non-moving cameras facing the target at different angles.
-* The video footage from all camera angles are synchronized and begin at the same point in real time.
+* The videos are shot from two or more non-moving cameras facing the target at different angles.
+* The videos from all camera angles are synchronized and begin at the same point in real time.
 * There are the same number of Blender cameras, positioned in the same orientation and relative position.
-* The Blender cameras are calibrated with respect to the focal length and any other relevant settings.
-* Your video has been tracked - there are virtual markers in Blender tracking each real world point of interest on your target.
-* Each track has an associated Empty object linked to its path (this is done using `Movie Clip Editor > Reconstruction > Link Empty to Track`).
+* The Blender cameras are calibrated with respect to focal length and sensor size.
+* Your videos has been tracked - there are virtual markers in Blender tracking each real world point of interest on your target.
+* The tracks for any given point of interest are named the same across all the videos.
+* Each track has an associated Empty object linked to its path on its corresponding camera (this is done using `Movie Clip Editor > Reconstruction > Link Empty to Track` while the desired camera is set to the default scene camera).
 
 ### During
 
-1. Mentally pick a single physical point to reconstruct.
-2. Select the Empty objects from each camera that track the physical point.
-3. Invoke the Resolve Camera Tracks operator from the menu or searchbar, as described above.
+1. Select all the Empty objects generated by all the invocations of `Movie Clip Editor > Reconstruction > Link Empty to Track` previously.
+2. Invoke the Resolve Camera Tracks operator from the menu or searchbar, as described above.
 
 ### After
 
-There should be a new Empty object with its location animated such that at every frame, it **tracks the physical marker in 3D**.
+There should be new Empty objects with their location animated such that at every frame, it **tracks the physical marker in 3D**.
 
-The resulting Empty object will only be keyframed when at least 2 of the Blender markers it is tracking are enabled - when its 3D position can be unambiguously determined.
+The resulting Empty objects will only be keyframed when at least 2 of the Blender markers they are tracking are enabled - when their 3D position can be unambiguously determined.
 
 Possible errors include:
 
-* `Non-empty object selected`: when invoking the operator, one of the selected objects was not of the type Empty.
+* `Non-empty object "SOME_OBJECT" selected`: SOME_OBJECT was not of the type Empty.
     * Select only empty objects.
-* `Motion Tracking constraint to be converted not found`: one of the selected objects was missing the Follow Track constraint that makes it follow the track.
-    * Ensure each object has a Follow Track constraint.
-    * Ensure the objects are empties created from motion tracks.
-* `Movie clip to use tracking data from isn't set`: one of the selected objects' Follow Track constraint did not have its Clip property set.
-    * Check the Follow Track constraint settings for each object to ensure the Clip property is set.
-    * Ensure the objects are empties created from motion tracks.
-* `Tracked object not found`: one of the selected objects' Follow Track constraint did not have a correctly associated track.
+* `At least two objects associated with tracks named "SOME_TRACK" required, only one selected`: there was only one object associated with tracks named SOME_TRACK.
+    * Select two or more Empty objects associated with tracks named SOME_TRACK.
+* `Follow Track constraint for "SOME_OBJECT" not found`: SOME_OBJECT was missing the Follow Track constraint that makes it follow a motion track.
+    * Ensure SOME_OBJECT has a Follow Track constraint.
+    * Ensure SOME_OBJECT is an Empty object created by `Movie Clip Editor > Reconstruction > Link Empty to Track`.
+* `Clip for constraint "SOME_CONSTRAINT" of "SOME_OBJECT" not found`: SOME_OBJECT's Follow Track constraint did not have its clip property set.
+    * Check the Follow Track constraint settings for SOME_OBJECT to ensure the clip property is set.
+    * Ensure SOME_OBJECT is an Empty object created by `Movie Clip Editor > Reconstruction > Link Empty to Track`.
+* `Track for constraint "SOME_CONSTRAINT" of "SOME_OBJECT" not found`: SOME_OBJECT's Follow Track constraint did not have an associated track.
     * Check the Follow Track constraint settings for each object to ensure the Track property is set.
     * Ensure the objects are empties created from motion tracks.
 * `At least 2 cameras need to be available`: the selected empties must represent views from two or more cameras, but only represented 0 or 1.
     * Select empties representing views from more than 1 camera.
     * Select more than 1 empty.
-* `Lines are too close to parallel`: the rays shot from the two cameras to their associated Empty objects are parallel.
+* `Lines are too close to parallel`: the rays shot from two or more cameras to their associated Empty objects are too close to parallel.
     * Shoot the footage from cameras at a larger angle apart.
     * Select only one empty per camera.
--- Resolve Camera Tracks.py
+++ Resolve Camera Tracks.py
@@ -1,13 +1,15 @@
 from mathutils import Vector
 import bpy
 import itertools
+import re
+from collections import defaultdict
 
 bl_info = {
     "name": "Resolve Camera Tracks",
     "author": "Anthony Zhang",
     "category": "Animation",
-    "version": (1, 0),
-    "blender": (2, 69, 0),
+    "version": (1, 1),
+    "blender": (2, 75, 0),
     "location": "View3D > Object > Resolve Camera Tracks or Search > Resolve Camera Tracks",
     "description": "3D point reconstruction from multiple camera angles",
 }
@@ -16,121 +18,163 @@ class ResolveCameraTracks(bpy.types.Operator):
     bl_idname = "animation.resolve_camera_tracks"
     bl_label = "Resolve Camera Tracks"
     bl_options = {"REGISTER", "UNDO"}  # enable undo for operator
-    
+
     def execute(self, context):
         targets = []
         for obj in context.selected_objects:
             if obj.type == "EMPTY":
                 targets.append(obj)
             else:
-                self.report({"ERROR_INVALID_INPUT"}, "Non-empty object selected")
+                self.report({"ERROR_INVALID_INPUT"}, "Non-empty object \"{}\" selected".format(obj.name))
                 return {"CANCELLED"}
-        
-        bpy.ops.clip.constraint_to_fcurve() # apply motion track for all targets
-        return self.add_resolved_empty(targets)
-    
-    def get_target_locations(self, target):
+
+        # associate pairs of empties together by base name
+        targets_by_track_name = defaultdict(list)
+        for target in targets:
+            track, _ = self.get_target_track(target)
+            targets_by_track_name[track.name].append(target)
+        for name, point_targets in targets_by_track_name.items():
+            if len(point_targets) < 2:
+                self.report({"ERROR_INVALID_INPUT"}, "At least two objects associated with tracks named \"{}\" required, only one selected".format(name))
+                return {"CANCELLED"}
+
+        # add the resolved empties
+        resolved_empties = []
+        for point_targets in targets_by_track_name.values():
+            try:
+                resolved_empties.append(self.add_resolved_empty(point_targets))
+            except Exception as e:
+                import traceback
+                self.report({"ERROR_INVALID_INPUT"}, traceback.format_exc())
+                return {"CANCELLED"}
+
+        # select the resolved empties
+        bpy.ops.object.select_all(action="DESELECT")
+        for empty in resolved_empties: empty.select = True
+
+        return {"FINISHED"}
+
+    def get_target_track(self, target):
         """
-        Obtains a list of positions in world space for a given target for each frame in its tracking clip.
-        
-        Returns the list of positions, the associated camera, the start frame, and the end frame.
+        Returns a motion tracking track associated with object `target` and the camera associated with object `target`.
         """
         # find the follow track constraint and obtain the associated track
-        for c in target.constraints:
-            if c.type == "FOLLOW_TRACK":
-                track_constraint = c
-        if not track_constraint:
-            raise Exception("Motion Tracking constraint to be converted not found")
-        if not track_constraint.clip:
-            raise Exception("Movie clip to use tracking data from isn't set")
-        tracks = (track for obj in track_constraint.clip.tracking.objects for track in obj.tracks) #wip: this is a workaround for when clip.tracking.tracks is empty when it shouldn't be
-        track = None
-        for t in tracks:
-            if t.name == track_constraint.track:
-                track = t
-        if track == None:
-            raise Exception("Tracked object not found")
-        
+        for constraint in target.constraints:
+            if constraint.type == "FOLLOW_TRACK":
+                track_constraint = constraint
+                if not track_constraint.clip:
+                    raise Exception("Clip for constraint \"{}\" of \"{}\" not found".format(track_constraint, target.name))
+                break
+        else:
+            raise Exception("Follow Track constraint for \"{}\" not found".format(target.name))
+
+        # get the track for the track constraint on the target
+        for track in track_constraint.clip.tracking.tracks:
+            if track.name == track_constraint.track:
+                return track, track_constraint.camera
+        raise Exception("Track for constraint \"{}\" of \"{}\" not found".format(track_constraint, target.name))
+
+    def get_target_locations(self, target):
+        """
+        Returns a list of positions in world space for object `target` for frames that it is animated for (and None for frames that are not), the camera associated with object `target`, the start frame, and the end frame.
+        """
+        track, camera = self.get_target_track(target)
+
         # obtain track information
-        marker_frames = {marker.frame for marker in track.markers if not marker.mute} # set of frames of enabled markers
+        marker_frames = {marker.frame for marker in track.markers if not marker.mute} # set of frame indices for enabled markers
         start_frame, end_frame = min(marker_frames), max(marker_frames)
-        
-        # store object world locations
+
+        original_frame = bpy.context.scene.frame_current # save the frame so we can restore it later
+
+        # store object world locations for each frame
         locations = []
-        print("start")
         for i in range(start_frame, end_frame + 1):
             if i in marker_frames:
                 bpy.context.scene.frame_set(i)
                 locations.append(target.matrix_world.to_translation())
             else:
                 locations.append(None)
-        return locations, track_constraint.camera, start_frame, end_frame
-    
+
+        bpy.context.scene.frame_set(original_frame) # move back to the original frame
+
+        return locations, camera, start_frame, end_frame
+
     def add_resolved_empty(self, targets):
         """
-        Adds an empty animated to stay at the point closest to every target in `targets`.
+        Adds an empty animated to be at the point closest to every target in `targets`, where each target in `targets` is animated by a Follow Track constraint.
+
+        Returns the newly created empty.
         """
-        
+
         # obtain target information
         target_points, target_cams, target_starts, target_ends = [], [], [], []
         for target in targets:
-            try:
-                points, cam, start, end = self.get_target_locations(target)
-            except Exception as e:
-                self.report({"ERROR_INVALID_INPUT"}, str(e))
-                return {"CANCELLED"}
+            points, cam, start, end = self.get_target_locations(target)
             target_points.append(points + [None]) # the last element must be None
             target_cams.append(cam)
             target_starts.append(start)
             target_ends.append(end)
-        
+
         if len(set(target_cams)) < 2: # two camera is the minimum number of cameras
-            self.report({"ERROR_INVALID_INPUT"}, "At least 2 cameras need to be available")
-            return {"CANCELLED"}
-        
+            raise Exception("At least 2 cameras need to be available")
+
         # add the empty object
         bpy.ops.object.add(type="EMPTY")
         resolved = bpy.context.active_object
-        
+
+        original_frame = bpy.context.scene.frame_current # save the frame so we can restore it later
+
         # set up keyframes for each location
-        original_frame = bpy.context.scene.frame_current
+        min_distance, min_distance_frame = float("inf"), None
+        max_distance, max_distance_frame = 0, None
         for frame in range(min(target_starts), max(target_ends)):
             # clamp indices to the last value, None, if outside of range
             indices = []
             for start, end in zip(target_starts, target_ends):
                 index = frame - start
                 indices.append(-1 if index < 0 or index > end - start else index)
-            
+
             bpy.context.scene.frame_set(frame) # move to the current frame
-            
+
             # go through each possible combination of targets and find the one that gives the best result
-            best_location = None
-            best_distance = float("inf")
+            best_location, best_distance = None, float("inf")
             for pair in itertools.combinations(range(0, len(targets)), 2):
                 first, second = pair[0], pair[1]
                 cam1, cam2 = target_cams[first].location, target_cams[second].location
                 location1, location2 = target_points[first][indices[first]], target_points[second][indices[second]]
-                if location1 != None and location2 != None:
-                    try:
-                        location, distance = closest_point(cam1, cam2, location1, location2)
-                    except Exception as e:
-                        self.report({"ERROR_INVALID_INPUT"}, str(e))
-                        return {"CANCELLED"}
+                if location1 is not None and location2 is not None:
+                    location, distance = closest_point(cam1, cam2, location1, location2)
                     if distance < best_distance: # better result than current best
                         best_distance = distance
                         best_location = location
-            
+
             # add keyframe if possible
             if best_location != None:
                 resolved.location = best_location
                 resolved.keyframe_insert(data_path="location")
+
+                if best_distance <= min_distance:
+                    min_distance = best_distance
+                    min_distance_frame = frame
+                if best_distance >= max_distance:
+                    max_distance = best_distance
+                    max_distance_frame = frame
+
         bpy.context.scene.frame_set(original_frame) # move back to the original frame
-        return {"FINISHED"}
+
+        # make the resolved track object more identifiable
+        track, _ = self.get_target_track(targets[0])
+        resolved.name = "{}_tracked".format(track.name)
+        resolved.empty_draw_type = "SPHERE"
+        resolved.empty_draw_size = 0.1
+
+        self.report({"INFO"}, "{}: min error {} (frame {}), max error {} (frame {})".format(resolved.name, min_distance / 2, min_distance_frame, max_distance / 2, max_distance_frame))
+        return resolved
 
 def closest_point(cam1, cam2, point1, point2):
     """
     Produces the point closest to the lines formed from `cam1` to `point1` and from `cam2` to `point2`, and the total distance between this point and the lines.
-    
+
     Reference: http://www.gbuffer.net/archives/361
     """
     dir1 = point1 - cam1
@@ -151,7 +195,7 @@ def closest_point(cam1, cam2, point1, point2):
 
 def add_object_button(self, context):
     self.layout.operator(ResolveCameraTracks.bl_idname,
-        text="Reconstruct Camera Tracks", icon="PLUGIN")
+        text="Resolve Camera Tracks", icon="PLUGIN")
 
 def register():
     bpy.utils.register_class(ResolveCameraTracks)
